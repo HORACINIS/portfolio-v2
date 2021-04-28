39d94517d76a60a3dd48e93cf1cbee08b2faa848
horacinis <horacinis@gmail.com>
2021-04-28 15:04:00 +1000

    created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ElevationScroll >
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography className={classes.title} variant="h6">Horacio Moran</Typography>
+            <section>
+              <Button className={classes.menuButton} color="inherit">Projects</Button>
+              <Button className={classes.menuButton} color="inherit">Skills</Button>
+              <Button className={classes.menuButton} color="inherit">About Me</Button>
+              <Button className={classes.menuButton} color="inherit">Contact Me</Button>
+            </section>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </React.Fragment>
+  );
+}
+
+export default Navbar;
