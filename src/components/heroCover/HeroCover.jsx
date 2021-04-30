4e9f658ef374943a5182824d068d805f3cbdc222
horacinis <horacinis@gmail.com>
2021-04-30 18:33:14 +1000

    added avatar and styled it

--- src/components/heroCover/HeroCover.jsx
+++ src/components/heroCover/HeroCover.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-
+import Avatar from '@material-ui/core/Avatar';
+import avatar from '../../images/avatar.jpg';
 import { useStyles } from './styles'
-
 
 const HeroCover = () => {
   const classes = useStyles();
   return (
-    <section className={classes.textContainer}>
+    <section className={classes.root}>
       <Grid container justify='center' direction='column'>
         <Typography
           className={classes.welcomeText}
           variant='h2'
           align='center'
         >
-          Welcome to my portfolio
+          Welcome to my portfolio.
         </Typography>
         <Typography
           className={classes.introText}
           variant='h5'
           align='center'
         >
-          I'm Horacio Moran, junior Software Developer
+          I'm Horacio Moran, junior Software Developer.
         </Typography>
+        <Grid>
+          <Avatar alt="Horacio" src={avatar} className={classes.avatar} />
+        </Grid>
       </Grid>
     </section>
   )
--- src/components/heroCover/styles.js
+++ src/components/heroCover/styles.js
@@ -1,14 +1,23 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 
-export const useStyles = makeStyles(() => ({
-  textContainer: {
+export const useStyles = makeStyles((theme) => ({
+  root: {
     height: '80vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: '15vh'
+    marginBottom: '15vh',
+    margin: theme.spacing(1),
   },
+  avatar: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 40,
+    filter: 'grayScale(90%)'
+  }
 
   // welcomeText: {
   //   color: 'purple',
