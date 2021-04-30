f9581d07e08b84ea46c78783ab480f00cbc6e46a
horacinis <horacinis@gmail.com>
2021-04-30 19:52:35 +1000

    changed navbar text colour

--- src/components/navbar/styles.js
+++ src/components/navbar/styles.js
@@ -4,11 +4,11 @@ export const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     // background: 'linear-gradient(#56CCF2, #2F80ED)',
-    background: '#2F80ED',
-
+    background: '#ffbe0b',
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    color: '#d90429'
   },
   title: {
     flexGrow: 1,
