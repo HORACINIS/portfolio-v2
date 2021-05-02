2ed63f68c7a70f35b46aa0ce7f0d289afc824950
horacinis <horacinis@gmail.com>
2021-05-02 16:33:09 +1000

    working on project and projects components, installed react icons

--- src/components/projects/projectsList.js
+++ src/components/projects/projectsList.js
@@ -1,9 +1,22 @@
-import el_cartel from './images/el_cartel.png'
+import { SiJavascript, SiReact } from 'react-icons/si'
+import { DiRuby } from 'react-icons/di';
+import el_cartel from './images/el_cartel.png';
 
 export const projectsList = [
   {
-    name: 'El Cartel',
+    id: '1',
+    title: 'El Cartel',
+    intro: 'Full stack two-sided marketplace created with Rails',
+    body: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
     img: { el_cartel },
-
+    iconsTechUsed: [SiJavascript, SiReact]
+  },
+  {
+    id: '2',
+    title: 'El Cartel DOS',
+    intro: 'Full stack two-sided marketplace created with Rails',
+    body: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+    img: { el_cartel },
+    iconsTechUsed: [SiJavascript, SiReact, DiRuby]
   }
 ]
