01e16d171fb130dbcece75d27a5018a5bbcda884
Horacinis Morancinis <horacinis@gmail.com>
2021-05-04 23:13:32 +1000

    changed background for about me component

--- src/components/aboutMe/style.js
+++ src/components/aboutMe/style.js
@@ -1,7 +1,11 @@
 export const tempStyle = {
   wrapper: {
-    backgroundImage: 'url(https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2557&q=80)',
+    backgroundImage: 'url(https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2600&q=80)',
+    // backgroundImage: 'url(https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2557&q=80)',
     // backgroundImage: 'url(https://images.unsplash.com/photo-1580408485028-225972105876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2610&q=80)',
+    // backgroundImage: 'url(https://images.unsplash.com/photo-1572666341285-c8cb9790ca50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80)',
+    // backgroundImage: 'url(https://images.unsplash.com/photo-1600478822888-e3d96cdc8ae2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+
     color: 'white',
     backgroundPosition: 'center',
     backgroundRepeat: 'noRepeat',
