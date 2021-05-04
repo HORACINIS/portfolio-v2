bf8b6e63916d8f3362fe06862c69223b9d8a25a9
Horacinis Morancinis <horacinis@gmail.com>
2021-05-04 18:54:50 +1000

    created the structure for the about me component

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import profilePic from '../../images/mainPic.jpg';
 
 const AboutMe = () => {
   return (
     <section id='about'>
       <h1>About Me</h1>
+      <img src={profilePic} alt='profile' />
+      <blockquote>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, corrupti ex. Adipisci voluptatum velit consequatur tempore recusandae nostrum quae, quaerat, ab tempora amet dolor esse nam facere quam omnis, nesciunt dolorem! Repellendus dolorum quo inventore quis illum, est quae iusto. Dolore optio quibusdam commodi, sequi corrupti natus adipisci laudantium hic?
+      </blockquote>
+      <h5>Horacio</h5>
+      <blockquote>
+        Lorem ipsum dolor sit amet consectetur.
+      </blockquote>
     </section>
   )
 }
