423c996196a4bf982cb17f8deec2c0fb1f0343f8
Horacinis <horacinis@gmail.com>
2021-05-05 15:52:34 +1000

    about me code with makeStyles

---
+++ src/components/aboutMe/AboutMeStyles.js
@@ -0,0 +1,101 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  root: {
+    backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2600&q=80)',
+    // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1580408485028-225972105876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2610&q=80)',
+    // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1572666341285-c8cb9790ca50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80)',
+    // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2557&q=80)',
+    // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1600478822888-e3d96cdc8ae2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+
+    color: 'white',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'noRepeat',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifContent: 'center',
+    textAlign: 'center',
+    width: '100%',
+    minHeight: '95vh'
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 800,
+    textTransform: 'uppercase',
+    letterSpacing: '1.5px'
+  },
+  profilePic: {
+    height: 200,
+    width: 200,
+    border: 'solid 2px #fff',
+    borderRadius: '50%',
+    // marginTop: '60px',
+    marginTop: '40px',
+  },
+  textContent: {
+    fontFamily: 'sans-serif',
+    fontSize: '20px',
+    marginTop: '50px'
+  },
+  name: {
+    textTransform: 'uppercase',
+    fontSize: '30px',
+    marginTop: '30px',
+    letterSpacing: '1.5px'
+  },
+  bottomText: {
+    fontSize: '17px'
+  }
+})
+
+// export const tempStyle = {
+//   root: {
+//     backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2600&q=80)',
+//     // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1580408485028-225972105876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2610&q=80)',
+//     // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1572666341285-c8cb9790ca50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80)',
+//     // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2557&q=80)',
+//     // backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1600478822888-e3d96cdc8ae2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+
+//     color: 'white',
+//     backgroundPosition: 'center',
+//     backgroundRepeat: 'noRepeat',
+//     backgroundSize: 'cover',
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     justifContent: 'center',
+//     textAlign: 'center',
+//     width: '100%',
+//     minHeight: '95vh'
+//   },
+//   title: {
+//     fontSize: 50,
+//     fontWeight: 800,
+//     textTransform: 'uppercase',
+//     letterSpacing: '1.5px'
+//   },
+//   profilePic: {
+//     height: 200,
+//     width: 200,
+//     border: 'solid 2px #fff',
+//     borderRadius: '50%',
+//     // marginTop: '60px',
+//     marginTop: '40px',
+//   },
+//   textContent: {
+//     fontFamily: 'sans-serif',
+//     fontSize: '20px',
+//     marginTop: '50px'
+//   },
+//   name: {
+//     textTransform: 'uppercase',
+//     fontSize: '30px',
+//     marginTop: '30px',
+//     letterSpacing: '1.5px'
+//   },
+//   bottomText: {
+//     fontSize: '17px'
+//   }
+// }
