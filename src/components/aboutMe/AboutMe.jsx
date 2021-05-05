4638fb240e78583aa14b4577248d2f51bc6c1969
HORACIO MORAN <horacinis@gmail.com>
2021-05-05 11:40:25 +1000

    touched up about me

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -5,19 +5,17 @@ import profilePic from '../../images/mainPic.jpg';
 const AboutMe = () => {
   return (
     <section id='about' style={tempStyle.root}>
-      <div style={tempStyle.testimonial}>
-        <article>
-          <h1 style={tempStyle.title}>About Me</h1>
-          <img src={profilePic} style={tempStyle.profilePic} alt='profile' />
-          <blockquote style={tempStyle.textContent}>
-            Lorem ipsum dolor sit amet consectetut conse dae nostrum quae, quaerat, ab tempora amet dolor esse nam facere quam omnis, nesciunt dolorem! Repellendus dolorum quo inventore quis illum, est quae iusto. Dolore optio quibusdam commodi, sequi corrupti natus adipisci laudantium hic?
+      <article>
+        <h1 style={tempStyle.title}>About Me</h1>
+        <img src={profilePic} style={tempStyle.profilePic} alt='profile' />
+        <blockquote style={tempStyle.textContent}>
+          Lorem ipsum dolor sit amet consectetut conse dae nostrum quae, quaerat, ab tempora amet dolor esse nam facere quam omnis, nesciunt dolorem! Repellendus dolorum quo inventore quis illum, est quae iusto. Dolore optio quibusdam commodi, sequi corrupti natus adipisci laudantium hic?
           </blockquote>
-          <h5 style={tempStyle.name}>Horacio</h5>
-          {/* <p style={tempStyle.bottomText}>
-            Lorem ipsum dolor sit amet consectetur.
-          </p> */}
-        </article>
-      </div>
+        <h5 style={tempStyle.name}>Horacio</h5>
+        <p style={tempStyle.bottomText}>
+          Lorem ipsum dolor sit amet consectetur.
+          </p>
+      </article>
     </section>
   )
 }
--- src/components/contactMe/ContactMe.jsx
+++ src/components/contactMe/ContactMe.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactMe = () => {
   return (
-    <section id='contact'>
+    <section id='contact' style={{ color: 'white' }}>
       <h1>Contact Me</h1>
     </section>
   )
