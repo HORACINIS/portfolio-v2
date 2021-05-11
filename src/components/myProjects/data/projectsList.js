0cd00f1fae95ae29e3d8b0e7c47c7aefec59b6d5
Horacinis <horacinis@gmail.com>
2021-05-11 14:41:19 +1000

    added the honey homage project

--- src/components/myProjects/data/projectsList.js
+++ src/components/myProjects/data/projectsList.js
@@ -1,6 +1,6 @@
 // icons of tech used
-import { SiJavascript, SiReact, SiRails, SiCss3, SiBootstrap, SiAmazonaws, SiPostgresql } from 'react-icons/si';
-import { DiRuby } from 'react-icons/di';
+import { SiJavascript, SiReact, SiRails, SiCss3, SiMongodb, SiBootstrap, SiAmazonaws, SiPostgresql } from 'react-icons/si';
+import { DiRuby, DiNodejsSmall } from 'react-icons/di';
 import { ImHtmlFive } from 'react-icons/im';
 import { FaCcStripe } from 'react-icons/fa';
 import { BsTerminalFill } from 'react-icons/bs';
@@ -10,6 +10,7 @@ import { IoIosConstruct } from 'react-icons/io';
 import el_cartel from '../images/el_cartel.jpg';
 import my_todo_list from '../images/my_todo_list.jpg';
 import lyrics_x from '../images/lyrics_x.jpg';
+import honey_homage from '../images/honey_homage.jpg';
 import portfolio_v1 from '../images/portfolio_v1.jpg';
 import horacinis_cafe from '../images/horacinis_cafe.jpg';
 import project_coming from '../images/project_coming.jpg';
@@ -19,6 +20,8 @@ const html = <ImHtmlFive />;
 const css = <SiCss3 />;
 const javaScript = <SiJavascript />;
 const react = <SiReact />;
+const node = <DiNodejsSmall />;
+const mongo = <SiMongodb />;
 const ruby = <DiRuby />;
 const rails = <SiRails />;
 const terminal = <BsTerminalFill />;
@@ -60,6 +63,16 @@ export const projectsList = [
     links: {
       siteLive: 'https://lyrics-finder-horacinis.netlify.app/',
       repoURL: 'https://github.com/HORACINIS/lyrics-finder'
+    }
+  },
+  {
+    title: 'Honey Homage',
+    intro: 'Full-stack marketplace, created with the MERN stack',
+    img: honey_homage,
+    iconsTechUsed: [html, css, javaScript, react, node, mongo],
+    links: {
+      siteLive: 'https://peaceful-reef-34834.herokuapp.com/',
+      repoURL: 'https://github.com/HORACINIS/honey_homage'
     }
   },
   {
