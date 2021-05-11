48f2f2bc9c4e6a1ec377c59349c807a9ba30d0cd
Horacinis <horacinis@gmail.com>
2021-05-12 00:22:03 +1000

    added the contact links for the bottom bar

--- src/components/nav/bottomNavbar/BottomNavbar.jsx
+++ src/components/nav/bottomNavbar/BottomNavbar.jsx
@@ -5,7 +5,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
-import { useStyles } from './style';
+import { useStyles } from './BottomNavbarStyles';
 
 
 const BottomNavbar = () => {
@@ -21,10 +21,10 @@ const BottomNavbar = () => {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction icon={<GitHubIcon className={classes.icons} />} />
-      <BottomNavigationAction icon={<TwitterIcon className={classes.icons} />} />
-      <BottomNavigationAction icon={<LinkedInIcon className={classes.icons} />} />
-      <BottomNavigationAction icon={<EmailIcon className={classes.icons} />} />
+      <BottomNavigationAction href='https://github.com/HORACINIS' target='_blank' icon={<GitHubIcon className={classes.icons} />} />
+      <BottomNavigationAction href='https://twitter.com/HORACINIS' target='_blank' icon={<TwitterIcon className={classes.icons} />} />
+      <BottomNavigationAction href='https://www.linkedin.com/in/horacio-moran-837a891b4/' target='_blank' icon={<LinkedInIcon className={classes.icons} />} />
+      <BottomNavigationAction href='mailto: horacinis@gmail.com' target='_blank' icon={<EmailIcon className={classes.icons} />} />
     </BottomNavigation>
   );
 }
