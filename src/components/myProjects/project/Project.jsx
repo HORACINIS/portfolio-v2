b5e83cec374df203c5f1172ee4a91f1c5846a9af
Horacinis <horacinis@gmail.com>
2021-05-15 20:20:37 +1000

    added hover effect to the projects

--- src/components/myProjects/project/Project.jsx
+++ src/components/myProjects/project/Project.jsx
@@ -10,6 +10,7 @@ import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { motion } from 'framer-motion';
 import { BiWorld } from 'react-icons/bi';
 import { SiGithub } from 'react-icons/si';
 import { useStyles } from './ProjectStyles';
@@ -23,62 +24,64 @@ const Project = ({ title, intro, body, img, iconsTechUsed, links, links: { siteL
   };
 
   return (
-    <Card className={classes.root} raised>
-      {/* <CardActionArea> */}
-      <CardHeader
-        // className={classes.header}
-        title={title} // title
-        subheader={intro}  // subtitle 
-      />
-      <CardMedia
-        className={classes.media}
-        image={img}  // image src 
-        title={title} // image alt
-      />
-      {/* <CardContent>
+    <motion.div whileHover={{ scale: 1.01 }} transition={{duration: 0.3}}>
+      <Card className={classes.root} raised>
+        {/* <CardActionArea> */}
+        <CardHeader
+          // className={classes.header}
+          title={title} // title
+          subheader={intro}  // subtitle 
+        />
+        <CardMedia
+          className={classes.media}
+          image={img}  // image src 
+          title={title} // image alt
+        />
+        {/* <CardContent>
         <Typography variant="body1" component="p">
           {body}
         </Typography>
       </CardContent> */}
-      {/* </CardActionArea> */}
-      <CardActions disableSpacing>
-        {iconsTechUsed.map((icon, index) => ( // icons of tech used
-          <IconButton className={classes.techIcons} key={index} aria-label='Tech used'>
-            {icon}
-          </IconButton>
-        ))}
-        {links &&
-          <IconButton
-            style={{ color: 'red' }}
-            className={clsx(classes.expand, {
-              [classes.expandOpen]: expanded,
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        }
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{siteLive || repoURL ? 'Links' : 'No Links'}</Typography>
-          {/* no siteLive icon will be displayed if there isn't a siteLive in the projectsList object file */}
-          {siteLive &&
-            <IconButton className={classes.linksIcons} aria-label='site live' target='_blank' href={siteLive}>
-              <BiWorld />
+        {/* </CardActionArea> */}
+        <CardActions disableSpacing>
+          {iconsTechUsed.map((icon, index) => ( // icons of tech used
+            <IconButton className={classes.techIcons} key={index} aria-label='Tech used'>
+              {icon}
+            </IconButton>
+          ))}
+          {links &&
+            <IconButton
+              style={{ color: 'red' }}
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
             </IconButton>
           }
-          {/* no github icon will be displayed if there isn't a repoURL in the projectsList object file */}
-          {repoURL &&
-            <IconButton className={classes.linksIcons} aria-label='repo url' target='_blank' href={repoURL}>
-              <SiGithub />
-            </IconButton>
-          }
-        </CardContent>
-      </Collapse>
-    </Card>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{siteLive || repoURL ? 'Links' : 'No Links'}</Typography>
+            {/* no siteLive icon will be displayed if there isn't a siteLive in the projectsList object file */}
+            {siteLive &&
+              <IconButton className={classes.linksIcons} aria-label='site live' target='_blank' href={siteLive}>
+                <BiWorld />
+              </IconButton>
+            }
+            {/* no github icon will be displayed if there isn't a repoURL in the projectsList object file */}
+            {repoURL &&
+              <IconButton className={classes.linksIcons} aria-label='repo url' target='_blank' href={repoURL}>
+                <SiGithub />
+              </IconButton>
+            }
+          </CardContent>
+        </Collapse>
+      </Card>
+    </motion.div>
   );
 }
 
