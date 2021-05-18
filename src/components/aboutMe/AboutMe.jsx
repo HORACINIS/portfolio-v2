b95572b4ad3156d53d772d8d8e5d6d0a977d2f99
Horacinis <horacinis@gmail.com>
2021-05-18 21:06:24 +1000

    added text content for the first part of the about me component

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -14,7 +14,7 @@ const AboutMe = () => {
         <Typography variant='h1' className={classes.title}>About Me</Typography>
         <img src={profilePic} className={classes.profilePic} alt='profile' />
         <Typography variant='subtitle1' className={classes.textContent}>
-          Lorem ipsum dolor sit amet consectetut conse dae nostrum quae, quaerat, ab tempora amet dolor esse nam facere quam omnis, nesciunt dolorem! Repellendus dolorum quo inventore quis illum, est quae iusto. Dolore optio quibusdam commodi, sequi corrupti natus adipisci laudantium hic?
+          I am a tech enthusiast, and it is the curiosity that drives me. To pursue my goal to become a full stack developer, I completed an intensive full-time bootcamp at Coder Academy Sydney, subsequently, a one-month internship at Leap Dev where I was part of a team and got to contribute towards a real-world project.
           </Typography>
         {/* <h5 className={classes.name}>Horacio</h5> */}
         {/* <p className={classes.bottomText}>
