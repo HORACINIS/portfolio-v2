8f37097145824c51f6b17e91b43c32f168c07e16
Horacinis <horacinis@gmail.com>
2021-05-19 22:33:51 +1000

    added lorem text and resume button to the about me section

--- src/components/myProjects/projects/ProjectsStyles.js
+++ src/components/myProjects/projects/ProjectsStyles.js
@@ -1,8 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-// const lightThemeImg = 'url(https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2534&q=80)';
 // const lightThemeImg = 'linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.75)), url(https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80)'
-
 
 
 // const lightThemeImg = 'url(https://images.unsplash.com/photo-1506526657667-dada52367f18?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80)'
@@ -23,8 +21,8 @@ export const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
     textAlign: 'center',
-    width: '100%',
-    // minHeight: '95vh',
+    // width: '100%',
+    maxWidth: '100vw'
   },
   title: {
     margin: '50px 0'
